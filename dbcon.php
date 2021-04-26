ea0798fd5babb155afbe33985bcf599007643112
sai5284 <71810351+sai5284@users.noreply.github.com>
2021-04-26 23:42:07 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,7 @@
+
+  <?php
+        $con = mysqli_connect('localhost', 'root', '', 'nursery2020');
+            if (!$con) {
+                die("database connection failed");
+            }
+        ?>
